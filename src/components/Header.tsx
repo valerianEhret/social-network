638e9fbd3d13f3a3dc611da2daea7d58b2af7fa7
valerianEhret <66052889+valerianEhret@users.noreply.github.com>
2020-05-29 22:05:10 +0200

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://i0.wp.com/tis39.ru/wp-content/uploads/2016/08/cropped-logo-tis_dolphin-and-starfish_-%D0%B7%D0%B2%D0%B5%D0%B7%D0%B4%D0%B0-%D0%B1%D0%B5%D0%B7-%D1%84%D0%BE%D0%BD%D0%B0.png?fit=1024%2C956" alt="picture" />
+    </header>
+  )
+}
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <main className="content">
+      <div>
+        <img src="https://www.ecos-office.com/fileadmin/_processed_/5/4/csm_Bielefeld_Kernkompetenz_1_b98104209a.jpg" alt="photo of city" />
+      </div>
+      <div>
+        <img src="https://greendestinations.org/wp-content/uploads/2019/05/avatar-exemple.jpg" alt="avatar" />
+      </div>
+      <div>
+        My posts
+      <div>
+          New posts
+      </div>
+        <div>post1</div>
+        <div>post2</div>
+      </div>
+    </main>
+  )
+}
+export default Profile;
