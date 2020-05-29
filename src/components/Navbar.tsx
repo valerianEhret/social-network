4486483de2ef6b1073eafdcc5063656a520723e4
valerianEhret <66052889+valerianEhret@users.noreply.github.com>
2020-05-29 22:53:35 +0200

    commit test2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="nav">
-        <div><a href="#">Profile</a></div>
-        <div><a href="#">Messages</a></div>
-        <div><a href="#">News</a></div>
-        <div><a href="#">Music</a></div>
-        <div><a href="#">Settings</a></div>
-      </nav>
+      <div><a href="#">Profile</a></div>
+      <div><a href="#">Messages</a></div>
+      <div><a href="#">News</a></div>
+      <div><a href="#">Music</a></div>
+      <div><a href="#">Settings</a></div>
+        Hello
+    </nav>
   )
 }
 export default Navbar;
