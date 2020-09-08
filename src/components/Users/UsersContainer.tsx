575591a561356682a44c043f6dc23ea2a10a143b
valerianEhret <vkadirovv@gmail.com>
2020-09-09 00:04:04 +0200

    Samurai put 62

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -43,7 +43,7 @@ class UsersAPIComponent extends React.Component<UsersDataStateType>{
 
     componentDidMount() {
         this.props.toggleIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {withCredentials:true}).then(response => {
 
             this.props.toggleIsFetching(false)
             this.props.setUsers(response.data.items)
@@ -54,7 +54,7 @@ class UsersAPIComponent extends React.Component<UsersDataStateType>{
     onPageChanged = (pageNumber: number) => {
         this.props.toggleIsFetching(true)
         this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {withCredentials:true}).then(response => {
 
             this.props.toggleIsFetching(false)
             this.props.setUsers(response.data.items)
