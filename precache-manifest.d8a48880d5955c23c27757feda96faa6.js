a7eafef407c05456df21e182416cc8dcf25d44cf
valerianEhret <vkadirovv@gmail.com>
2021-09-10 21:02:46 +0200

    Updates

---
+++ precache-manifest.d8a48880d5955c23c27757feda96faa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba254f9cc93e54ad37a98b29d6765a50",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "ab11abc994bfc390dcaf",
+    "url": "/social-network/static/css/main.ae886291.chunk.css"
+  },
+  {
+    "revision": "ad2298dae9613d484d51",
+    "url": "/social-network/static/js/2.f339d274.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.f339d274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab11abc994bfc390dcaf",
+    "url": "/social-network/static/js/main.e0dffaff.chunk.js"
+  },
+  {
+    "revision": "68b01136d249e5809ba9",
+    "url": "/social-network/static/js/runtime-main.bf681233.js"
+  },
+  {
+    "revision": "be00fc4a29d03016e78b28c9943e3f51",
+    "url": "/social-network/static/media/preloader.be00fc4a.svg"
+  }
+]);
